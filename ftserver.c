b5c9df68a1c73a198ce91070533accfa16c0afa2
James Wilson <wilsjame@oregonstate.edu>
2018-05-30 23:01:07 -0700

    server side: add get directory function

--- ftserver.c
+++ ftserver.c
@@ -12,10 +12,13 @@
 #include <netdb.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h>
 
 void printUsage();
+int getDirectory(char* contents[]);
 int handleRequest(int sockfd, int* data_port);
-void receivePayload(int sockfd, char* buffer, size_t size);
+void receiveMessage(int sockfd, char* buffer, size_t size);
+int receiveNumber(int sockfd);
 int startup(char* server_port);
 
 int main(int argc, char* argv[])
@@ -44,7 +47,6 @@ int main(int argc, char* argv[])
 
 		// Accept a connection, blocking until one is available.
 		// accept() generates a new socket to be used for the actual connection.
-		printf("trace1\n");
 		controlConnection_sockfd = accept(server_sockfd, NULL, NULL);
 		if(controlConnection_sockfd < 0) perror("ERROR on accept!\n"); 
 
@@ -58,14 +60,31 @@ int main(int argc, char* argv[])
 				perror("fork() failure!\n"); exit(1);
 			
 			case 0: 	// In child. 
-				printf("Contact made! In child process!\n");
+				printf("Connection made with client! In child process!\n");
 
-				int command;
+				//int dataConnection_sockfd;
+
+				// Accept and interpret command from client.
 				int data_port;
-				int dataConnection_sockfd;
+				int command;
+				command = handleRequest(controlConnection_sockfd, &data_port);
 
-				handleRequest(controlConnection_sockfd, &data_port);
+				// Check what command the client sent and act accordingly.
+				if(command == 1)
+				{
+					printf("List directory requested on port %d\n", data_port);
+				}
+				else if(command == 2)
+				{
+					printf("File requested.\n");
+				}
+				else if(command == 0)
+				{
+					perror("ERROR invalid command!");
+				}
+				
 
+				
 				break;
 				
 		}
@@ -82,34 +101,114 @@ void printUsage()
 	printf("Usage ./chatclient <SERVER_PORT>\n");
 }
 
-// Handles the request from the client.
+// Gets the contents of a directory and stores them in the parameter passed.
+// Returns the length in bytes of the sum of the regular file names.
+int getDirectory(char* contents[])
+{
+	DIR *dirp;
+	struct dirent *entry;
+	int total_length = 0;
+	int number_of_files = 0;
+	int i = 0;
+
+	// Open the current directory.
+	dirp = opendir(".");
+
+	if(dirp != NULL)
+	{
+
+		// Begin getting directory contents.
+		while((entry = readdir(dirp)) != NULL)
+		{
+			
+			// Only care about regular files.
+			if(entry->d_type == DT_REG)
+			{
+
+				// Store the name of the files in the array of strings
+				// and add its byte length to the total length.
+				contents[i] = entry->d_name;
+				total_length += strlen(contents[i]);
+				i++;
+			}
+
+		}
+
+		number_of_files = i - 1;
+	}
+
+	closedir(dirp);
+
+	return number_of_files + total_length;
+		
+}
+
+// Handles the request from the client by parsing the message.
+// Returns 1 for "-l", 2 for "-g", and 0 otherwise. 
 int handleRequest(int sockfd, int* data_port)
 {
-	char buffer[100]; memset(buffer, '\0', sizeof(buffer));
-	char client_command[3]; memset(client_command, '\0', sizeof(client_command));
+	char command[3] = "\0";
 
-	// Receive entire message from client
-	receivePayload(sockfd, buffer, sizeof(buffer));
+	// Get command and port number from clients message.
+	receiveMessage(sockfd, command, 3);
+	*data_port = receiveNumber(sockfd);
 
-	// *****************LEFT OFF HERE*********************
-	printf("Request received from client: %s\n", buffer);
+	if(strcmp(command, "-l") == 0)
+	{
+		printf("trace -l\n");
+
+		return 1;
+
+	}
+	else if(strcmp(command, "-g") == 0)
+	{
+		printf("trace -g\n");
+
+		return 2;
+
+	}
+	else
+	{
+
+		// Invalid argument!
+		return 0;
+
+	}
+											
+}
+
+// Read client's message from the socket  
+// and stores it in payload.
+void receiveMessage(int sockfd, char* payload, size_t size)
+{
+	char buffer[size + 1]; memset(buffer, '\0', sizeof(buffer));
+	ssize_t n;
+	size_t total = 0;
+
+	while(total < size)
+	{
+		n = read(sockfd, buffer + total, size - total);
+		total += n;
+
+		if( n < 0) perror("ERROR receiving message\n");
+	}
+
+	strncpy(payload, buffer, size);
 
 }
 
-// Read client's message, up to size bytes, from the socket.
-void receivePayload(int sockfd, char* buffer, size_t size)
+// Read client's message that is known to contain a number.
+int receiveNumber(int sockfd)
 {
-	char tempBuffer[1]; memset(buffer, '\0', sizeof(tempBuffer));
-	int bytes_read;
+	int number;
+	ssize_t n = 0;
 
-	do
-	{
-		bytes_read = recv(sockfd, tempBuffer, sizeof(tempBuffer), 0); 
-		strcat(buffer, tempBuffer);
+	n = read(sockfd, &number, sizeof(int));
 
-	}while(bytes_read > 0);
+	if( n < 0) perror("ERROR receiving number\n");
 
-	buffer[strlen(buffer)-1] = '\0';
+	return number;
+
 }
 
 // Does all the dirty work to start up the server on the given port.
